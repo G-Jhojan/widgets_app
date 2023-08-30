db4572fdd185f27ea88a99c84ffd1c8a476d8c50
Jhojan <jhojanrock98@gmail.com>
2023-08-29 21:34:02 -0500

    eso

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.red,
+  Colors.redAccent,
+  Colors.blue,
+  Colors.blueAccent,
+  Colors.teal,
+  Colors.indigo,
+  Colors.indigoAccent,
+  Colors.purple,
+  Colors.purpleAccent,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(
+            selectedColor >= 0, 'El color selccionado no puede ser mayor a 0'),
+        assert(
+            selectedColor < colorList.length, 'El color selccionado no puede ser menor o igual a ${colorList.length -1}');
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[selectedColor]
+  );
+}
