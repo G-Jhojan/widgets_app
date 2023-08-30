b2f7568a2d23c0067c83ee149fb6071f364f7e34
Jhojan <jhojanrock98@gmail.com>
2023-08-30 12:23:54 -0500

    cambios realizados

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({
+    required this.title,
+    required this.subTitle,
+    required this.link,
+    required this.icon,
+  });
+}
+
+const appMenuItems = <MenuItem>[
+
+  MenuItem(
+    title: 'Botones', 
+    subTitle: 'Varios Botones en Flutter', 
+    link: '/butttons', 
+    icon: Icons.ac_unit
+    ),
+                        
+      MenuItem(
+    title: 'Tarjetas', 
+    subTitle: 'Un contendedor estilizado', 
+    link: '/card', 
+    icon: Icons.credit_card
+    )
+];
